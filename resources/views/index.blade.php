6589138f0c01ccf19561c99201e181440d8f99f4
Manasseh Banda <manassehbanda@aol.com>
2017-10-07 12:55:54 -0500

    Profile for Heroku

---
+++ resources/views/index.blade.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ideathon | Hackathon Idae Generator </title>
+    <meta name="description" content="Arisn - Multi Purpose HTML5 Template">
+    <meta name="author" content="premonday.com">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i|Poppins:300,400,500,600" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" href={{asset("assets/favicon.ico")}} type="image/x-icon" />
+    <link rel="shortcut icon" href={{asset("assets/favicon.ico")}} type="image/x-icon" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href={{asset("assets/vendor/reset.css")}}>
+    <link rel="stylesheet" href={{asset("assets/vendor/bootstrap/bootstrap.min.css")}}>
+    <link rel="stylesheet" href={{asset("assets/vendor/ion-icons/ionicons.min.css")}}>
+    <link rel="stylesheet" href={{asset("assets/vendor/owl-slider/owl.carousel.css")}}>
+    <link rel="stylesheet" href={{asset("assets/vendor/slideshow/slideshow.css")}}>
+    <link rel="stylesheet" href={{asset("assets/vendor/lightbox/lity.min.css")}}>
+    <!-- Theme CSS -->
+    <link id="theme" rel="stylesheet" href={{asset("assets/css/theme.css")}}>
+    <script src={{asset("assets/vendor/modernizr.js")}}></script>
+</head>
+
+<body class="indexing">
+
+<!-- Scroll up -->
+<div class="scrollup">
+    <i class="ion-ios-arrow-up"></i>
+</div>
+
+<!-- Main Container -->
+<div class="web-in">
+
+    <!-- Start Project Modal -->
+    <div id="modal-bg">
+        <img src={{asset("assets/img/exit.svg")}} class="exit" alt="">
+        <div class="holder">
+            <div class="placer">
+                <div id="modal" class="center">
+                    <div class="title">
+                        <h5>Download Arisn</h5>
+                        <span class="promo-heading">You've made the right choice</span>
+                        <hr>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 w-section"><i class="ion-ios-star-outline"></i>
+                            <h3>A download link will appear here soon.</h3>
+                            <p>Thank you for your patience.
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <a href="#" class="btn"><span>Place for link</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- Header -->
+    <header id="sp1" class="center dark1" style="height:570px; background-image:url(img/fp-img.png)">
+        <div class="header-in">
+            <div class="caption">
+                <h5>Welcome to Ideathon</h5>
+                <h1>Hackathon Idea Generator</h1>
+                <h6></h6>
+                <p>Try it out :D</p>
+            </div>
+            <img class="arrow" src="img/arrow.svg" alt="">
+        </div>
+    </header>
+
+    <section class="dark1">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col col-md-6 col-md-offset-3 title">
+                    <h2>Idea Categories</h2>
+                    <h4>Ideas generated will fall into one of these 4 categories.</h4>
+                </div>
+            </div>
+
+            <div class="row m-space">
+                <div class="col-sm-3 col-xs-6 right mh">
+                    <div class="aligner">
+                        <div class="holder">
+                            <i class="ion-gear-a"></i>
+                            <h5>Normal</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p>
+                            <hr>
+                            <i class="ion-alert"></i>
+                            <h5>Kinda Normal</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 center phones mh hidden-xs">
+                    <img class="behind" style="width: 80%" src={{asset("assets/img/trump.png")}} alt="">
+                </div>
+                <div class="col-sm-3 col-xs-6 mh">
+                    <div class="aligner">
+                        <div class="holder">
+                            <i class="ion-alert"></i>
+                            <h5>Crazy</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p>
+                            <hr>
+                            <i class="ion-alert-circled"></i>
+                            <h5>Batshit Crazy</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="smart">
+        <div class="container">
+            <div class="row foot-space">
+                <div class="col-md-4 col-sm-12">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Odio, consequatur facere molestiae iusto atque.
+                    </p>
+                    <div class="social">
+                        <a href="#"><i class="ion-social-twitter"></i></a>
+                        <a href="#"><i class="ion-social-googleplus"></i></a>
+                        <a href="#"><i class="ion-social-instagram-outline"></i></a>
+                        <a href="#"><i class="ion-social-linkedin-outline"></i></a>
+                        <a href="#"><i class="ion-social-pinterest-outline"></i></a>
+                        <a href="#"><i class="ion-social-github"></i></a>
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <div class="col-xs-12 xs-center">
+                    <p class="footer-text">Copyright &copy; Ideathon. All rights reservered.</p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</div>
+<!-- end of web-in -->
+<script src={{asset("assets/vendor/jquery-2.2.1.min.js")}}></script>
+<script src={{asset("assets/vendor/matchHeight-min.js")}}></script>
+<script src={{asset("assets/vendor/contact/validator.js")}}></script>
+<script src={{asset("assets/vendor/contact/contact.js")}}></script>
+<script src={{asset("assets/vendor/pace.min.js")}}></script>
+<script src={{asset("assets/vendor/headroom/headroom.min.js")}}></script>
+<script src={{asset("assets/vendor/owl-slider/owl.carousel.min.js")}}></script>
+<script src={{asset("assets/vendor/slideshow/anime.min.js")}}></script>
+<script src={{asset("assets/vendor/slideshow/imagesloaded.pkgd.min.js")}}></script>
+<script src={{asset("assets/vendor/slideshow/main.js")}}></script>
+<script src={{asset("assets/vendor/parallax/materialize.min.js")}}></script>
+<script src={{asset("assets/vendor/lightbox/lity.min.js")}}></script>
+<script src={{asset("assets/vendor/tabs/jquery.tabslet.min.js")}}></script>
+<script src={{asset("assets/vendor/masonry.pkgd.min.js")}}></script>
+<script src={{asset("assets/js/main.min.js")}}></script>
+</body>
+
+</html>
